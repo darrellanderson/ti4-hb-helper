b8c1351d9a785ec03d0e1b47ce4b919927c36fa8
darrell <darrell.anderson@gmail.com>
2025-08-19 19:54:20 -0700

    remove .d.ts files

--- src/lib/gen/gen-faction-reference/gen-faction-reference.d.ts
+++
@@ -1,6 +0,0 @@
-import { HomebrewModuleType } from "ti4-ttpg-ts-types";
-import { AbstractGen } from "../abstract-gen/abstract-gen";
-export declare class GenFactionReference extends AbstractGen {
-    constructor(homebrew: HomebrewModuleType);
-    generate(errors: Array<string>): Promise<void>;
-}
