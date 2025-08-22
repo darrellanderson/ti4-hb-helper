fa7fb2b08fb784baa6b4068ea5b5de1889bc0d08
darrell <darrell.anderson@gmail.com>
2025-08-21 21:34:03 -0700

    progress

---
+++ src/generate.ts
@@ -0,0 +1,14 @@
+import { HomebrewModuleType } from "ti4-ttpg-ts-types";
+import { GenAll } from "./lib/gen/gen-all/gen-all";
+
+export async function generate(homebrew: HomebrewModuleType): Promise<void> {
+  const genAll = new GenAll(homebrew);
+  const errors: Array<string> = [];
+  await genAll.generate(errors);
+  if (errors.length > 0) {
+    console.error("Errors occurred during generation:");
+    errors.forEach((error) => console.error(` - ${error}`));
+    return;
+  }
+  genAll.writeOutputFiles();
+}
