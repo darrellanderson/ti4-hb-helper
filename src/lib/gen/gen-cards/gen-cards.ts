6b402d19e20b6cc67ad6bb88b41224cedfb32316
darrell <darrell.anderson@gmail.com>
2025-08-27 21:31:27 -0700

    add event cards

--- src/lib/gen/gen-cards/gen-cards.ts
+++ src/lib/gen/gen-cards/gen-cards.ts
@@ -14,6 +14,7 @@ import path from "path";
 const CARD_TYPES_PORTRAIT: Array<string> = [
   "action",
   "agenda",
+  "event",
   "exploration-cultural",
   "exploration-hazardous",
   "exploration-frontier",
